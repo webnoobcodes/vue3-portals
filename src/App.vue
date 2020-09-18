d9469431490cbd695977c3a906185d460e27e88a
Mario <support@webnoobcodes.com>
2020-09-18 12:16:56 +0200

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button @click='showNotification'>Trigger Notification!</button>
+  <Teleport v-if="isOpen" to='#notification-target'>
+    <div class='notification'>
+      This is a notification!
+    </div>
+  </Teleport>
 </template>
 
 <script>
@@ -8,8 +14,25 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data: () => {
+    return {
+      isOpen: false,
+      timeout: false
+    }
+  },
   components: {
     HelloWorld
+  },
+  methods: {
+    showNotification() {
+      this.isOpen = true;
+
+      clearTimeout(this.timeout)
+
+      this.timeout = setTimeout(() => {
+        this.isOpen = false
+      }, 3000)
+    }
   }
 }
 </script>
@@ -22,5 +45,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#notification-target {
+  position: absolute;
+  top: 16px;
+  right: 16px;
+}
+
+.notification {
+  width: 300px;
+  padding: 32px;
+  margin-bottom: 16px;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1.4rem;
+  color: #fefefe;
+  background-color: #333;
 }
 </style>
